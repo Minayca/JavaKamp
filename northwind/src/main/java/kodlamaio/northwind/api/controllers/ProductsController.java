22df0af34d750b7b9d329ff532b6066e13236962
Minayca <minaycakanberk@gmail.com>
2021-05-23 20:55:22 +0300

    22.05.2021

--- northwind/src/main/java/kodlamaio/northwind/api/controllers/ProductsController.java
+++ northwind/src/main/java/kodlamaio/northwind/api/controllers/ProductsController.java
@@ -4,28 +4,37 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import kodlamaio.northwind.business.abstracts.ProductService;
+import kodlamaio.northwind.core.utilities.results.DataResult;
+import kodlamaio.northwind.core.utilities.results.Result;
 import kodlamaio.northwind.entities.concretes.Product;
 
-@RestController
+@RestController //Diğer ortamlarda kodun tanınmasını sağlar.
 @RequestMapping("/api/products")
 public class ProductsController {
 	
 	private ProductService productService;
 	
-	@Autowired
+	@Autowired //ProductService'i implemente edenleri new'liyor.
 	public ProductsController(ProductService productService) {
 		super();
 		this.productService = productService;
 	}
 
 
-
 	@GetMapping("/getall")
-	public List<Product> getAll(){
+	public DataResult<List<Product>> getAll(){
 		return this.productService.getAll();
+	}
+	
+	@PostMapping("/add")
+	//RequestBody product'ın body'sini map'ler.
+	public Result add(@RequestBody Product product) {
+		return this.productService.add(product);
 	}
 }
